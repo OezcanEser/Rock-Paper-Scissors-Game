9872da545bdcc7730b72fba83673bcc0276b1c1d
OezcanEser <oe.eser84@gmail.com>
2021-05-20 14:33:27 +0200

    start

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Js Vertiefung - Projekt
+// Projekt lev3_9: Rock, Paper, Scissors
+
+// Aufgabenstellung
+
+// Jeder hat in seinem Leben schonmal "Rock, Paper, Scissors" (Schnick, Schnack, Schnuck) gespielt. Ihr kennt die Regeln also bereits. ;) 
+
+// Definiere, wie viele Runden wir gegen den Computer spielen werden.
+// Die Farbe sollte sich ändern, wenn es sich um einen Gewinn, ein Unentschieden oder eine Niederlage handelt. Das Design kannst du natürlich auch anders gestalten. 
